baf50a7404919773924cebe61462a5b893956edb
Lih Chen <lihster@gmail.com>
2013-11-12 19:11:09 -0800

    First commit

---
+++ src/com/polymorphic/simpletimer/MainActivity.java
@@ -0,0 +1,53 @@
+package com.polymorphic.simpletimer;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.polymorphic.simpletimer";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //android.support.v7.app.ActionBar bar = getSupportActionBar();
+        //bar.setNavigationMode(android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
