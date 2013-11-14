2de1faabac0289e5a5e9f85f010287c336b9682b
Lih Chen <lihster@gmail.com>
2013-11-14 13:09:13 -0800

    Got numpad working.

--- src/com/polymorphic/simpletimer/MainActivity.java
+++ src/com/polymorphic/simpletimer/MainActivity.java
@@ -1,53 +1,138 @@
 package com.polymorphic.simpletimer;
 
-import android.app.ActionBar;
+
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE = "com.polymorphic.simpletimer";
+  public final static String TAG = "MainActivity";
+  public final static String EXTRA_MESSAGE = "com.polymorphic.simpletimer";
+  public enum State {
+    FIRST_DIGIT, SECOND_DIGIT;
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //android.support.v7.app.ActionBar bar = getSupportActionBar();
-        //bar.setNavigationMode(android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS);
+  private TextView currTimeTextView;
+  private State currState;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    //android.support.v7.app.ActionBar bar = getSupportActionBar();
+    //bar.setNavigationMode(android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS);
+    if (currTimeTextView == null) {
+      currTimeTextView = (TextView) findViewById(R.id.hour_text_view);
     }
+    currState = State.FIRST_DIGIT;
+  }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-        return super.onCreateOptionsMenu(menu);
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  public void sendMessage(View view) {
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = (EditText) findViewById(R.id.edit_message);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivity(intent);
+  }
+
+  private void clearTimerBorderHelper(int victimId, int selectedId) {
+    TextView currView =
+      ((TextView) findViewById(victimId));
+    if (selectedId != currView.getId()) {
+      Drawable background = currView.getBackground();
+      if (background != null) {
+        currView.setBackgroundResource(0);
+      }
     }
+  }
 
-    public void sendMessage(View view) {
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+  private void clearTimerBorder(int selectedId) {
+    clearTimerBorderHelper(R.id.hour_text_view, selectedId);
+    clearTimerBorderHelper(R.id.minute_text_view, selectedId);
+    clearTimerBorderHelper(R.id.second_text_view, selectedId);
+  }
+
+  public void onTimeTextClick(View view) {
+    selectTimeText(view);
+  }
+
+  private void selectTimeText(View view) {
+    currTimeTextView = (TextView) view;
+    clearTimerBorder(view.getId());
+    view.setBackgroundResource(R.drawable.timer_border);
+  }
+
+  private String digitBuilder(String input) {
+    String first_digit;
+    if (currState == State.FIRST_DIGIT)
+      first_digit = "0";
+    else
+      first_digit = Character.valueOf(currTimeTextView.getText().charAt(1)).toString();
+
+    StringBuilder sb = new StringBuilder(2);
+    sb.append(first_digit);
+    sb.append(input);
+    String rStr = sb.toString();
+    int currTimeTextId = currTimeTextView.getId();
+    if (currTimeTextId == R.id.minute_text_view || currTimeTextId == R.id.second_text_view) {
+      if (Integer.parseInt(rStr) > 59) {
+        rStr = "59";
+      }
     }
+    return rStr;
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_search:
-                //openSearch();
-                return true;
-            case R.id.action_settings:
-                //openSettings();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+  private void transitionState() {
+    switch (currState) {
+      case FIRST_DIGIT:
+        currState = State.SECOND_DIGIT;
+        break;
+      case SECOND_DIGIT:
+        if( currTimeTextView.getId() == R.id.hour_text_view) {
+          selectTimeText(findViewById(R.id.minute_text_view));
+        } else if (currTimeTextView.getId() == R.id.minute_text_view) {
+          selectTimeText(findViewById(R.id.second_text_view));
+        } else if (currTimeTextView.getId() == R.id.second_text_view) {
         }
+        currState = State.FIRST_DIGIT;
+        break;
     }
+  }
+
+  public void onNumPadClick(View view) {
+    String input = ((TextView)view).getText().toString();
+    Log.d(TAG, input);
+    currTimeTextView.setText(digitBuilder(input));
+    transitionState();
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle presses on the action bar items
+    switch (item.getItemId()) {
+      case R.id.action_search:
+        //openSearch();
+        return true;
+      case R.id.action_settings:
+        //openSettings();
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
 }
